aa2f4b906ee2efcd214e681813aae8e61b1351e1
Max Maetze <maetze.max@icloud.com>
2024-01-17 22:26:04 +0100

    Initial Commit

---
+++ Rjosk/RjoskApp.swift
@@ -0,0 +1,17 @@
+//
+//  RjoskApp.swift
+//  Rjosk
+//
+//  Created by Max Maetze on 17.01.24.
+//
+
+import SwiftUI
+
+@main
+struct RjoskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
