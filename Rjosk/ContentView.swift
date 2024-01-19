ff17bf524d47161aa3792f326d3efe9d672ce391
Max Maetze <maetze.max@icloud.com>
2024-01-19 13:25:03 +0100

    adjusted y- axis scaling

--- Rjosk/ContentView.swift
+++ Rjosk/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Rjosk
-//
-//  Created by Max Maetze on 17.01.24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -54,45 +47,35 @@ struct ChartView: View {
     
     var body: some View {
         VStack {
-            if entries.isEmpty {
-                ProgressView()
-            } else {
-                HStack(spacing: axisSpacing) {
-                    ZStack {
-                        let scaledEntries = applyScaling(chartEntries: entries)
-                        ForEach(Array(entries.enumerated()), id: \.offset) { entry in
-                            if entry.offset == 0 {
-                                getLabel(posY: scaledEntries[entry.offset].y, valueY: entry.element.y)
-                            }
-                            
-                            if let maxEntry = entries.max(by: { $0.y < $1.y }), maxEntry == entry.element {
-                                getLabel(posY: scaledEntries[entry.offset].y, valueY: maxEntry.y)
-                            }
-                            
-                            if let minEntry = entries.min(by: { $0.y < $1.y }), minEntry == entry.element {
-                                getLabel(posY: scaledEntries[entry.offset].y, valueY: minEntry.y)
-                            }
+            HStack(spacing: axisSpacing) {
+                ZStack {
+                    let scaledEntries = applyScaling(chartEntries: entries)
+                    ForEach(Array(entries.enumerated()), id: \.offset) { entry in
+                        if let maxEntry = entries.max(by: { $0.y < $1.y }), maxEntry == entry.element {
+                            getLabel(posY: scaledEntries[entry.offset].y, valueY: maxEntry.y)
+                        }
+                        
+                        if let minEntry = entries.min(by: { $0.y < $1.y }), minEntry == entry.element {
+                            getLabel(posY: scaledEntries[entry.offset].y, valueY: minEntry.y)
                         }
                     }
-                    .frame(width: axisWidth, height: height)
-                    
-                    ZStack {
-                        drawXAxis()
-                            .stroke(.primary, lineWidth: 3)
-                        
-                        drawLeftYAxis()
-                            .stroke(.primary, lineWidth: 3)
-                        
-                        drawLine()
-                            .stroke(.green, lineWidth: 3)
-                    }
-                    .frame(width: width - axisWidth, height: height)
                 }
-                .frame(width: width, height: height)
+                .frame(width: axisWidth, height: height)
+                
+                ZStack {
+                    drawXAxis()
+                        .stroke(.primary, lineWidth: 3)
+                    
+                    drawLeftYAxis()
+                        .stroke(.primary, lineWidth: 3)
+                    
+                    drawLine()
+                        .stroke(.green, lineWidth: 3)
+                }
+                .frame(width: width - axisWidth, height: height)
             }
+            .frame(width: width, height: height)
         }
-        .transition(.slide)
-        .animation(.bouncy, value: entries)
     }
     
     func getLabel(posY: CGFloat, valueY: CGFloat) -> some View {
@@ -161,11 +144,10 @@ struct ChartView: View {
     
     func drawLeftYAxis() -> Path {
         Path { path in
-            // Move to the bottom leading corner
             path.move(
                 to: CGPoint(
                     x: 0,
-                    y: height
+                    y: 0
                 )
             )
             
@@ -173,24 +155,28 @@ struct ChartView: View {
             path.addLine(
                 to: CGPoint(
                     x: 0,
-                    y: 0
+                    y: height
                 )
             )
         }
     }
     
     func applyScaling(chartEntries: [ChartEntry]) -> [ChartEntry] {
+        let minX = chartEntries.min(by: { $0.x < $1.x })?.x ?? 0
+        let minY = chartEntries.min(by: { $0.y < $1.y })?.y ?? 0
+        
         let maxX = chartEntries.max(by: { $0.x < $1.x })?.x ?? width
         let maxY = chartEntries.max(by: { $0.y < $1.y })?.y ?? height
         
         let chartWidth: CGFloat = width - axisSpacing - axisWidth
+        let chartHeight: CGFloat = height
         
-        let scaleRatioWidth = chartWidth / maxX
-        let scaleRatioHeight = height / maxY
+        let scaleRatioWidth = chartWidth / (maxX - minX)
+        let scaleRatioHeight = chartHeight / (maxY - minY)
         
         let result = chartEntries.map {
-            let x = $0.x * scaleRatioWidth
-            let y = $0.y * scaleRatioHeight
+            let x = ($0.x - minX) * scaleRatioWidth
+            let y = ($0.y - minY) * scaleRatioHeight
             
             return ChartEntry(x: x, y: y)
         }
