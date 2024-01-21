344062e626a47d72f691c8c281bde3ddcae9138a
Max Maetze <maetze.max@icloud.com>
2024-01-21 04:02:21 +0100

    fixed project structure and xcode crash

---
+++ Rjosk/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import RjoskChart
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ChartView()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
